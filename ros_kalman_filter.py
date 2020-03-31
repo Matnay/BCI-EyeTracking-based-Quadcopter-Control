caba80e57b9024e8bbd444e6d2b052595aee181b
Pranay Mathur <37473401+Matnay@users.noreply.github.com>
2020-03-31 22:28:30 +0530

    Add files via upload

---
+++ ros_kalman_filter.py
@@ -0,0 +1,52 @@
+import rospy
+from std_msgs.msg import Int32,Float32,String
+from math import *
+import numpy as np
+import time
+class Kalman:
+	def __init__(self):
+		self.action_1=0
+		self.data_sub=rospy.Subscriber("chatter",Int32,self.data_cb)
+		self.final_pub=rospy.Publisher("action",Float32,queue_size=1)
+
+	def gaussian(self,mu,sigma_sq,input_x):
+		coeff=1.0/sqrt(2*pi*sigma_sq)
+		expo=exp(-0.5*(x-mu)**2/sigma_sq)
+		return coeff* expo
+
+	def update(self,mean_1,var_1,mean_2,var_2):
+		new_mean=(var_2*mean_1+var_1*mean_2)/(var_1+var_2)
+		new_var=1/(1/var_1 + 1/var_2)
+		return [new_mean,new_var]
+
+	def predict(self,mean_1,var_1,mean_2,var_2):
+		new_mean=mean_1+mean_2
+		new_var=var_1+var_2
+		print("func")
+		return [new_mean,new_var]
+
+	def data_cb(self,msg):
+			global action_1
+			self.action_1=msg.data
+			return self.action_1
+
+	def initialisation(self):
+		rospy.init_node("kalman")
+		rate=rospy.Rate(2)
+		measurement_sig = 4
+		motion_sig = 2
+		mu = 1
+		sig = 10000.
+		while not rospy.is_shutdown():
+			mu, sig = kalman.update(mu, sig, self.action_1, measurement_sig)
+			print('Update: [{}, {}]'.format(mu, sig))
+			mu, sig = kalman.predict(mu, sig, self.action_1, motion_sig)
+			print('Predict: [{}, {}]'.format(mu, sig))
+			time.sleep(0.5)
+			self.final_pub.publish(mu)
+		print('\n')
+		print('Final result: [{}, {}]'.format(mu, sig))
+
+if __name__ == '__main__':
+	kalman=Kalman()
+	kalman.initialisation()
