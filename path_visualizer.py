44b1cba442b95a588db19a7a4c4c659134b43e91
Pranay Mathur <37473401+Matnay@users.noreply.github.com>
2020-04-15 09:18:30 +0530

    Update path_visualizer.py

--- path_visualizer.py
+++ path_visualizer.py
@@ -7,41 +7,59 @@ from geometry_msgs.msg import PoseStamped,Pose
 
 path = Path()
 path_square=Path()
+action=0
 def odom_cb(data):
 	global path
+	global action
 	path.header = data.header
 	pose = PoseStamped()
 	pose.header = data.header
 	pose.pose = data.pose.pose
 	path.poses.append(pose)
 	path_pub.publish(path)
+	
+def action_cb(msg):
+	global action
+	action=msg.data
 
+# use the terminal command example: rostopic pub /topic std_msgs/Int32 1
+#change number according to each action
 
 def square(iteration):
 	global path_sq
 	path_square.header.stamp =rospy.Time.now() 
 	pose_sq = PoseStamped()
 	path_square.header.frame_id="map"
-	if(iteration<=10):
+	if(action==0):
 		pose_sq.pose.position.x=0
 		pose_sq.pose.position.y=0
 		pose_sq.pose.position.z=2
-	if(iteration>=10 and iteration<20):
+		action_pub.publish(1)
+
+	if(action==1):
 		pose_sq.pose.position.x=5
 		pose_sq.pose.position.y=0
 		pose_sq.pose.position.z=2
-	if(iteration>=20 and iteration<30):
+		action_pub.publish(2)
+		
+	if(action==2):
 		pose_sq.pose.position.x=5
 		pose_sq.pose.position.y=5
 		pose_sq.pose.position.z=2
-	if(iteration>=30 and iteration<40):
+		action_pub.publish(3)
+
+	if(action==3):
 		pose_sq.pose.position.x=0
 		pose_sq.pose.position.y=5
 		pose_sq.pose.position.z=2
-	if(iteration>=40 and iteration<50):
+		action_pub.publish(4)
+
+	if(action==4):
 		pose_sq.pose.position.x=0
 		pose_sq.pose.position.y=0
 		pose_sq.pose.position.z=2
+		action_pub.publish(5)
+		
 	path_square.poses.append(pose_sq)
 	path_pub_sq.publish(path_square)
 
@@ -49,9 +67,12 @@ rospy.init_node('path_node')
 rate=rospy.Rate(10)
 odom_sub = rospy.Subscriber('mavros/local_position/odom', Odometry, odom_cb)
 path_pub = rospy.Publisher('/path', Path, queue_size=10)
+action_sub=rospy.Subscriber('topic',Int32,action_cb)
 path_pub_sq=rospy.Publisher('/path_square', Path, queue_size=10)
+action_pub=rospy.Publisher('action',Int32,queue_size=1)
 if __name__ == '__main__':
 	count=1
+	error=0
 	while not rospy.is_shutdown():
 		count=count+1
 		square(count)
