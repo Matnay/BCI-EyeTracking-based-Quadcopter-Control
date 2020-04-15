478d1247f24c29b8425e3217e8d3e8227d967f56
Pranay Mathur <37473401+Matnay@users.noreply.github.com>
2020-04-15 09:10:10 +0530

    Update Utility.py

--- Utility.py
+++ Utility.py
@@ -10,35 +10,37 @@ from std_msgs.msg import Int32
 class Utility:
 	def __init__(self):
 		self.odom_sub = rospy.Subscriber('mavros/local_position/odom', Odometry, self.odom_cb)
-		self.act_sub=rospy.Subscriber("action",Int32,act_cb)
+		self.act_sub=rospy.Subscriber('action',Int32,self.act_cb)
 		self.action=0
 		self.time_initial=rospy.Time.now()
+		self.error=1
+		self.pose_st = PoseStamped()
 
 	def act_cb(self,msg):
 		self.action=msg.data
 
 	def odom_cb(self,data):
-		pose_st = PoseStamped()
-		pose_st.header = data.header
-		pose_st.pose = data.pose.pose
-		if(self.action==2):
-			error=error+abs(pose_st.pose.position.y)
-		if(self.action==3):
-			error=error+abs(pose_st.pose.position.x-5)
-		if(self.action==4)
-			error=error+abs(pose_st.pose.position.y-5)
-		if(self.action==5)	
-			error=error+abs(pose_st.pose.position.x)
+		self.pose_st.header = data.header
+		self.pose_st.pose = data.pose.pose
 
-		u=(1/error)/(rospy.Time.now()-self.time_initial)	
-		print(("Utility: "{}).format(u))	
+	def check(self):
+		if(self.action==2):
+			self.error=self.error+abs(self.pose_st.pose.position.y)
+		if(self.action==3):
+			self.error=self.error+abs(self.pose_st.pose.position.x-5)
+		if(self.action==4):
+			self.error=self.error+abs(self.pose_st.pose.position.y-5)
+		if(self.action==5):	
+			self.error=self.error+abs(self.pose_st.pose.position.x)
+		u=(1/self.error)
+		print(("Utility: {}").format(u))	
+
+	def initialization(self):
+		while not rospy.is_shutdown():
+			self.check()
 
 if __name__ == '__main__':
 	rospy.init_node('error_node')
 	rate=rospy.Rate(10)
-	
-	while not rospy.is_shutdown():
-		utility=Utility()
-		rate.sleep()
-
-
+	utility=Utility()	
+	utility.initialization()
